b095a50496f430cce9a0851eb95f5a88104194c0
Prashanth R <103203393+Prashanth-4503@users.noreply.github.com>
2025-03-12 08:55:47 +0530

    Add files via upload

---
+++ Ex_3/ex_3_server.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+const PUBLIC_DIR = path.join(__dirname, 'public');
+
+const server = http.createServer((req, res) => {
+    let filePath = path.join(PUBLIC_DIR, req.url === '/' ? 'ex_3.html' : req.url);
+    
+    // Get file extension
+    let extname = path.extname(filePath);
+    
+    // Set content type
+    let contentType = 'text/html';
+    if (extname === '.css') contentType = 'text/css';
+    if (extname === '.js') contentType = 'text/javascript';
+
+    // Read and serve the file
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('<h1>404 - Not Found</h1>');
+            } else {
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
